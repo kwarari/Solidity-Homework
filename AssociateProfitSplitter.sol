8910b99ae052cc5d577ed11a927286cf3a4f67b4
unknown <kwarari@yahoo.com>
2021-07-14 22:57:12 -0700

    solutions

--- AssociateProfitSplitter.sol
+++ AssociateProfitSplitter.sol
@@ -1,34 +1,33 @@
 pragma solidity ^0.5.0;
-
-// lvl 1: equal split
 contract AssociateProfitSplitter {
-    address payable account_one = 0x418C695B050470DdD3deC5Db602585F92a7c2198;
-    address payable account_two = 0x0f1d8D56Ea512785973916519890869FD862783E;
-    address payable account_two = 0xa12F86B1383BF5aAc2D90594D77F3AD1A29215d6;
- 
-    constructor(address payable _one, address payable _two, address payable _three) public {
+    // @TODO: Create three payable addresses representing `employee_one`, `employee_two` and `employee_three`.
+    address payable employee_one;
+    address payable employee_two;
+    address payable employee_three;
+    
+       constructor(address payable _one, address payable _two, address payable _three) public {
         employee_one = _one;
         employee_two = _two;
         employee_three = _three;
-    }
-
-    function balance() public view returns(uint) {
+       }
+        
+        function balance() public view returns(uint) {
         return address(this).balance;
-    }
+        }
 
     function deposit() public payable {
         // @TODO: Split `msg.value` into three
-        uint amount = ; // Your code here!
-
+        uint amount = msg.value/3; // Your code here!
         // @TODO: Transfer the amount to each employee
-        // Your code here!
-
+        employee_one.transfer(amount);
+        employee_two.transfer(amount);
+        employee_three.transfer(amount);
         // @TODO: take care of a potential remainder by sending back to HR (`msg.sender`)
-        // Your code here!
+        msg.sender.transfer(msg.value-amount*3);
+        
     }
-
-    function() external payable {
+ function() external payable {
         // @TODO: Enforce that the `deposit` function is called in the fallback function!
-        // Your code here!
+        
     }
 }
